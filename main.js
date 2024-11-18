9cff6e4f4bf08627d2988e41e7b1863c48bb7eae
Renan Couto <renanmv10@hotmail.com>
2024-11-18 15:03:06 -0300

    manipulating the texts displayed on the site

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// Menu Section
+
+
 document.querySelector("#open-nav-menu").addEventListener("click", function(){
     document.querySelector("header nav .wrapper").classList.add("nav-open");
 })
@@ -5,4 +8,21 @@ document.querySelector("#open-nav-menu").addEventListener("click", function(){
 document.querySelector("#close-nav-menu").addEventListener("click", function(){
     document.querySelector("header nav .wrapper").classList.remove("nav-open");
 })
+
+var customer = "John";
+
+console.log(customer)
+
+// Greeting Section
+
+const greetingText = "Good Afternoon"
+const weatherCondition = "sunny";
+const userLocation = "New York";
+let temperature = 22.4391;
+let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)}°C outside.`;
+
+console.log(weatherText);
+
+document.querySelector("#greeting").innerHTML = greetingText;
+document.querySelector("p#weather").innerHTML = weatherText;
 
